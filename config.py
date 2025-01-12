522cfbf6a4e34f6fb611c26e156b4a84860d4b2e
fanes-setiawan <fanessetiawa.1401@gmail.com>
2025-01-12 16:47:30 +0700

    docs : swagger

--- config.py
+++ config.py
@@ -1,10 +1,24 @@
 
-from flask import Flask
+from flask import Flask,request
 from flask_sqlalchemy import SQLAlchemy 
 from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
-CORS(app,resources={r"/api/*": {"origins": "*"}},supports_credentials=True)
+# CORS(app,resources={r"/api/*": {"origins": "*"}},supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
+Swagger(app, template_file="swagger.yaml")
+
+@app.before_request
+def handle_options_request():
+    if request.method == "OPTIONS":
+        response = app.response_class()
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://secret123:2wsx1qaz@localhost:3306/restaurant_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
